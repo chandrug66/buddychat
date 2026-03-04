016fed44857e967f95d7e380c2acff2c1a8cfc8d
chandrug66 <chandrugunjalli4566@gmail.com>
2026-03-05 02:16:52 +0530

    update allowed hosts

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l+y0la91(6a%w*afudcux2(+ow3)-7-sd^lu8p3(#)p&*bfczq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["buddychat-iriz.onrender.com"]
 
 
 # Application definition
